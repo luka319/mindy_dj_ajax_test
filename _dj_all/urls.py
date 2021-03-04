cc4acfab5ea51eb47330182e8b9d67578cb5240e
Luka Pacholli <luka319@i.ua>
2021-03-04 22:52:51 +0200

    first commit

---
+++ _dj_all/urls.py
@@ -0,0 +1,24 @@
+"""_dj_all URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+import mindydb.views as mi_views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', mi_views.front_insert, name='front_in'),
+    path('insert_into/', mi_views.insert_into, name='insert_in'),
+]
---
+++ py_tut.bat
@@ -0,0 +1,3 @@
+@REM "W:\(__work_all)\(__part2)\Mindy_support_3mart2021\_dj_all\venv\Scripts\python.exe" %*
+
+"W:\(__work_all)\(__part2)\Mindy_support_3mart2021\_dj_all\venv\Scripts\python.exe" %*
